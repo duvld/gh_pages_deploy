1eacf315a1d4fa035c296d8c75ed454b0059647b
duvld <ollejna@gmail.com>
2019-02-22 15:14:49 -0500

    a

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import AppBar from 'material-ui/AppBar';
+import { render } from "react-dom";
+import "./styles/styles.css";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import VideoComponent from './VideoComponent';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
-export default App;
+let dom = document.getElementById("app");
+render(
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+            <div>
+            <AppBar title="React Twilio Video" />
+            <VideoComponent />
+            </div>
+    </MuiThemeProvider>
+    ,
+    dom
+);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+import React, { Component } from "react";
+import AppBar from 'material-ui/AppBar';
+import { render } from "react-dom";
+import "./index.css";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import VideoComponent from './VideoComponent';
+import injectTapEventPlugin from "react-tap-event-plugin";
+injectTapEventPlugin();
+
+let dom = document.getElementById("app");
+render(
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+            <div>
+            <AppBar title="React Twilio Video" />
+            <VideoComponent />
+            </div>
+    </MuiThemeProvider>
+    ,
+    dom
+);
